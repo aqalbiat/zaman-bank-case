4d43babf9e8fad56b3ee1a4da775fdc37a2c0a18
Aman <qalbiataman@gmail.com>
2025-10-18 15:13:02 +0500

    needed models for chat completion object added

--- testing/model.go
+++ testing/model.go
@@ -1,6 +1,22 @@
 package main
 
+type ReturnedMessage struct {
+	Content     string   `json:"content"`
+	Role        string   `json:"role"`
+	Annotations []string `json:"annotations"`
+}
+
 type ReturnedChoices struct {
+	FinishReason     string            `json:"finish_reason"`
+	Index            string            `json:"index"`
+	Message          ReturnedMessage   `json:"message"`
+	ProviderSpecific map[string]string `json:"provider_specific_fields"`
+}
+
+type ReturnedUsage struct {
+	CompletionTokens string `json:"completion_tokens"`
+	PromptTokens     string `json:"prompt_tokens"`
+	TotalTokens      string `json:"total_tokens"`
 }
 
 type ChatCompletionResponse struct {
@@ -10,4 +26,6 @@ type ChatCompletionResponse struct {
 	Object            string            `json:"object"`
 	SystemFingerprint string            `json:"system_fingerprint"`
 	Choices           []ReturnedChoices `json:"choices"`
+	Usage             []ReturnedUsage   `json:"usage"`
+	ServiceTier       string            `json:"service_tier"`
 }
